70eac7b308b95f89758c08ac18e44fc5f475da2b
Oleksandr Bazylevych <baz.production@gmail.com>
2020-03-28 21:56:22 +0200

    cli arguments

--- src/Parser.php
+++ src/Parser.php
@@ -13,21 +13,21 @@ class Parser implements ConsoleApplicationInterface
     public function run(array $options = []): void
     {
         $defaultOptions = [
-            'parse' => '',
-            'max' => 10,
+            CLI_ARGUMENT_NAME_PARSE => '',
+            CLI_ARGUMENT_NAME_MAX => 10,
         ];
 
         $options = array_merge($defaultOptions, $options);
 
-        if (!filter_var($options['parse'], FILTER_VALIDATE_URL)) {
+        if (!filter_var($options[CLI_ARGUMENT_NAME_PARSE], FILTER_VALIDATE_URL)) {
             throw new \RuntimeException('Invalid url');
         }
 
-        if (!filter_var((int) $options['max'], FILTER_VALIDATE_INT)) {
+        if (!filter_var((int) $options[CLI_ARGUMENT_NAME_MAX], FILTER_VALIDATE_INT)) {
             throw new \RuntimeException('Invalid max value');
         }
 
         $parser = (new ParsersStrategy())->get(ForumoduaParser::TYPE_NAME);
-        $parser->parse($options['parse'], $options['max']);
+        $parser->parse($options[CLI_ARGUMENT_NAME_PARSE], (int) $options[CLI_ARGUMENT_NAME_MAX]);
     }
 }
